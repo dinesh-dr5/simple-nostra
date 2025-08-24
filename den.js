4b667ce6220afcabe5ce5f51af8883c3bc11934b
dinesh-dr5 <a.dineshkumardon@gmail.com>
2025-08-24 21:11:58 +0530

    Upated the text

---
+++ den.js
@@ -0,0 +1,8 @@
+var offer =document.querySelector(".offer__section")
+var close =document.querySelector(".offerclose")
+
+close.addEventListener("click",function()
+{
+    offer.classList.add("hidden")
+})
+
